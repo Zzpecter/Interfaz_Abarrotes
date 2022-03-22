b6f5bfa7b99d89be2f8f2ceefa6cb366b14a89a6
renevs <rene.vilar@jala-foundation.org>
2022-03-22 09:20:55 -0400

    ADDED: Form de Unidades Presentacion

---
+++ DataLayer/Models/UnidadPresentacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+
+    public class ViUnidadPresentacion
+    {
+        public int id_unidad_presentacion { get; set; }
+        public string nombre_medida { get; set; }
+        public Double multiplicador_kg { get; set; }
+    }
+
+    public class UnidadPresentacion : ViUnidadPresentacion
+    {
+        public string usuario_registro { get; set; }
+    }
+}
---
+++ DataLayer/Tasks/UnidadPresentacion.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+
+namespace DataLayer.Tasks
+{
+    public class UnidadPresentacion
+    {
+        public static async Task<List<Models.ViUnidadPresentacion>> listar()
+        {
+            var response = await RequestController.SendHttpRequest(
+                HttpMethod.Get,
+                Globals.URL_UNIDAD_PRESENTACION,
+                String.Empty,
+                Globals.HTTP_HEADERS);
+
+            string responseText = await response.Content.ReadAsStringAsync();
+            List<Models.ViUnidadPresentacion> unidadPresentacion = JsonConvert.DeserializeObject<List<Models.ViUnidadPresentacion>>(responseText);
+            return unidadPresentacion;
+        }
+
+        public static async Task<int> insertar(Models.UnidadPresentacion unidadPresentacion)
+        {
+            string jsonBody = JsonConvert.SerializeObject(unidadPresentacion);
+
+            var response = await RequestController.SendHttpRequest(
+                HttpMethod.Post,
+                Globals.URL_UNIDAD_PRESENTACION,
+                jsonBody,
+                Globals.HTTP_HEADERS);
+            return ((int)response.StatusCode);
+        }
+
+        public static async Task<int> actualizar(Models.UnidadPresentacion unidadPresentacion, int idUnidadPresentacion)
+        {
+            string jsonBody = JsonConvert.SerializeObject(unidadPresentacion);
+            string url = Globals.URL_UNIDAD_PRESENTACION + "/" + idUnidadPresentacion.ToString();
+
+            var response = await RequestController.SendHttpRequest(
+                HttpMethod.Put,
+                url,
+                jsonBody,
+                Globals.HTTP_HEADERS);
+            return ((int)response.StatusCode);
+        }
+
+        public static async Task<Models.ViUnidadPresentacion> seleccionar(int idUnidadPresentacion)
+        {
+            string url = Globals.URL_UNIDAD_PRESENTACION + "/" + idUnidadPresentacion.ToString();
+            var response = await RequestController.SendHttpRequest(
+                HttpMethod.Get,
+                url,
+                String.Empty,
+                Globals.HTTP_HEADERS);
+
+            string responseText = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Models.ViUnidadPresentacion>(responseText);
+        }
+
+        public static async Task<int> eliminar(int idUnidadPresentacion)
+        {
+            string url = Globals.URL_UNIDAD_PRESENTACION + "/" + idUnidadPresentacion.ToString();
+
+            var response = await RequestController.SendHttpRequest(
+                HttpMethod.Delete,
+                url,
+                String.Empty,
+                Globals.HTTP_HEADERS);
+            return ((int)response.StatusCode);
+        }
+    }
+}
