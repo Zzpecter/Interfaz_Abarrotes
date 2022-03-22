e9bd077a428af37c7430582569d6dbf32c856733
renevs <rene.vilar@jala-foundation.org>
2022-03-22 10:28:11 -0400

    WIP Form Presentacion Producto

---
+++ DataLayer/Models/PresentacionProducto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class ViPresentacionProducto
+    {
+        public int id_presentacion_producto { get; set; }
+        public int id_unidad_presentacion { get; set; }
+        public string nombre_presentacion { get; set; }
+    }
+
+    public class PresentacionProducto : ViPresentacionProducto
+    {
+        public string usuario_registro { get; set; }
+    }
+}
---
+++ DataLayer/Tasks/PresentacionProducto.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+
+namespace DataLayer.Tasks
+{
+    public class PresentacionProducto
+    {
+        public static async Task<List<Models.ViMotivo>> listar()
+        {
+            var response = await RequestController.SendHttpRequest(
+                HttpMethod.Get,
+                Globals.URL_MOTIVOS,
+                String.Empty,
+                Globals.HTTP_HEADERS);
+
+            string responseText = await response.Content.ReadAsStringAsync();
+            List<Models.ViMotivo> motivos = JsonConvert.DeserializeObject<List<Models.ViMotivo>>(responseText);
+            return motivos;
+        }
+
+        public static async Task<int> insertar(Models.Motivo motivo)
+        {
+            string jsonBody = JsonConvert.SerializeObject(motivo);
+
+            var response = await RequestController.SendHttpRequest(
+                HttpMethod.Post,
+                Globals.URL_MOTIVOS,
+                jsonBody,
+                Globals.HTTP_HEADERS);
+            return ((int)response.StatusCode);
+        }
+
+        public static async Task<int> actualizar(Models.Motivo motivo, int idMotivo)
+        {
+            string jsonBody = JsonConvert.SerializeObject(motivo);
+            string url = Globals.URL_MOTIVOS + "/" + idMotivo.ToString();
+
+            var response = await RequestController.SendHttpRequest(
+                HttpMethod.Put,
+                url,
+                jsonBody,
+                Globals.HTTP_HEADERS);
+            return ((int)response.StatusCode);
+        }
+
+        public static async Task<Models.ViMotivo> seleccionar(int idMotivo)
+        {
+            string url = Globals.URL_MOTIVOS + "/" + idMotivo.ToString();
+            var response = await RequestController.SendHttpRequest(
+                HttpMethod.Get,
+                url,
+                String.Empty,
+                Globals.HTTP_HEADERS);
+
+            string responseText = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Models.ViMotivo>(responseText);
+        }
+
+        public static async Task<int> eliminar(int idMotivo)
+        {
+            string url = Globals.URL_MOTIVOS + "/" + idMotivo.ToString();
+
+            var response = await RequestController.SendHttpRequest(
+                HttpMethod.Delete,
+                url,
+                String.Empty,
+                Globals.HTTP_HEADERS);
+            return ((int)response.StatusCode);
+        }
+    }
+}
