d0994667cc8b1b5b0a2eeb7feda11746b823ba06
renevs <rene.vilar@jala-foundation.org>
2022-03-30 07:19:44 -0400

    ADDED: FrmLocalidades funcionando.

---
+++ UI/FrmLocalidades.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace UI
+{
+    partial class FrmLocalidades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLocalidades));
+            this.cmbDepartamento = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bVolver = new System.Windows.Forms.Button();
+            this.tbNombreLocalidad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bCancelar = new System.Windows.Forms.Button();
+            this.bGuardar = new System.Windows.Forms.Button();
+            this.dgvLocalidades = new System.Windows.Forms.DataGridView();
+            this.bElimiar = new System.Windows.Forms.Button();
+            this.bActualizar = new System.Windows.Forms.Button();
+            this.bAgregar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLocalidades)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbDepartamento
+            // 
+            this.cmbDepartamento.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDepartamento.FormattingEnabled = true;
+            this.cmbDepartamento.Location = new System.Drawing.Point(228, 369);
+            this.cmbDepartamento.Name = "cmbDepartamento";
+            this.cmbDepartamento.Size = new System.Drawing.Size(273, 32);
+            this.cmbDepartamento.TabIndex = 64;
+            this.cmbDepartamento.SelectedIndexChanged += new System.EventHandler(this.cmbDepartamento_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 372);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 24);
+            this.label1.TabIndex = 63;
+            this.label1.Text = "Departamento:";
+            // 
+            // bVolver
+            // 
+            this.bVolver.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.bVolver.Image = global::UI.Properties.Resources.volver;
+            this.bVolver.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bVolver.Location = new System.Drawing.Point(13, 427);
+            this.bVolver.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.bVolver.Name = "bVolver";
+            this.bVolver.Size = new System.Drawing.Size(139, 45);
+            this.bVolver.TabIndex = 62;
+            this.bVolver.Text = "Volver";
+            this.bVolver.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bVolver.UseVisualStyleBackColor = false;
+            // 
+            // tbNombreLocalidad
+            // 
+            this.tbNombreLocalidad.Enabled = false;
+            this.tbNombreLocalidad.Location = new System.Drawing.Point(228, 323);
+            this.tbNombreLocalidad.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tbNombreLocalidad.Name = "tbNombreLocalidad";
+            this.tbNombreLocalidad.Size = new System.Drawing.Size(273, 28);
+            this.tbNombreLocalidad.TabIndex = 54;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 323);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 24);
+            this.label3.TabIndex = 55;
+            this.label3.Text = "Nombre Localidad:";
+            // 
+            // bCancelar
+            // 
+            this.bCancelar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.bCancelar.Image = global::UI.Properties.Resources.cancelar;
+            this.bCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bCancelar.Location = new System.Drawing.Point(528, 259);
+            this.bCancelar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.bCancelar.Name = "bCancelar";
+            this.bCancelar.Size = new System.Drawing.Size(139, 45);
+            this.bCancelar.TabIndex = 60;
+            this.bCancelar.Text = "Cancelar";
+            this.bCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bCancelar.UseVisualStyleBackColor = false;
+            this.bCancelar.Visible = false;
+            this.bCancelar.Click += new System.EventHandler(this.bCancelar_Click);
+            // 
+            // bGuardar
+            // 
+            this.bGuardar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.bGuardar.Image = global::UI.Properties.Resources.guardar;
+            this.bGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bGuardar.Location = new System.Drawing.Point(528, 208);
+            this.bGuardar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.bGuardar.Name = "bGuardar";
+            this.bGuardar.Size = new System.Drawing.Size(139, 45);
+            this.bGuardar.TabIndex = 59;
+            this.bGuardar.Text = "Guardar";
+            this.bGuardar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bGuardar.UseVisualStyleBackColor = false;
+            this.bGuardar.Visible = false;
+            this.bGuardar.Click += new System.EventHandler(this.bGuardar_Click);
+            // 
+            // dgvLocalidades
+            // 
+            this.dgvLocalidades.AllowUserToAddRows = false;
+            this.dgvLocalidades.AllowUserToDeleteRows = false;
+            this.dgvLocalidades.AllowUserToResizeColumns = false;
+            this.dgvLocalidades.AllowUserToResizeRows = false;
+            this.dgvLocalidades.BackgroundColor = System.Drawing.Color.White;
+            this.dgvLocalidades.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvLocalidades.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.dgvLocalidades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLocalidades.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvLocalidades.Location = new System.Drawing.Point(13, 12);
+            this.dgvLocalidades.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dgvLocalidades.Name = "dgvLocalidades";
+            this.dgvLocalidades.RowHeadersVisible = false;
+            this.dgvLocalidades.RowHeadersWidth = 51;
+            this.dgvLocalidades.RowTemplate.Height = 29;
+            this.dgvLocalidades.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgvLocalidades.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvLocalidades.Size = new System.Drawing.Size(488, 293);
+            this.dgvLocalidades.TabIndex = 61;
+            this.dgvLocalidades.SelectionChanged += new System.EventHandler(this.dgvLocalidades_SelectionChanged);
+            // 
+            // bElimiar
+            // 
+            this.bElimiar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.bElimiar.Enabled = false;
+            this.bElimiar.Image = global::UI.Properties.Resources.eliminar;
+            this.bElimiar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bElimiar.Location = new System.Drawing.Point(528, 111);
+            this.bElimiar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.bElimiar.Name = "bElimiar";
+            this.bElimiar.Size = new System.Drawing.Size(139, 45);
+            this.bElimiar.TabIndex = 58;
+            this.bElimiar.Text = "Eliminar";
+            this.bElimiar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bElimiar.UseVisualStyleBackColor = false;
+            this.bElimiar.Click += new System.EventHandler(this.bElimiar_Click);
+            // 
+            // bActualizar
+            // 
+            this.bActualizar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.bActualizar.Enabled = false;
+            this.bActualizar.Image = global::UI.Properties.Resources.editar;
+            this.bActualizar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bActualizar.Location = new System.Drawing.Point(528, 60);
+            this.bActualizar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.bActualizar.Name = "bActualizar";
+            this.bActualizar.Size = new System.Drawing.Size(139, 45);
+            this.bActualizar.TabIndex = 57;
+            this.bActualizar.Text = "Editar";
+            this.bActualizar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bActualizar.UseVisualStyleBackColor = false;
+            this.bActualizar.Click += new System.EventHandler(this.bActualizar_Click);
+            // 
+            // bAgregar
+            // 
+            this.bAgregar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.bAgregar.Image = global::UI.Properties.Resources.agregar;
+            this.bAgregar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bAgregar.Location = new System.Drawing.Point(528, 12);
+            this.bAgregar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.bAgregar.Name = "bAgregar";
+            this.bAgregar.Size = new System.Drawing.Size(139, 45);
+            this.bAgregar.TabIndex = 56;
+            this.bAgregar.Text = "Agregar";
+            this.bAgregar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bAgregar.UseVisualStyleBackColor = false;
+            this.bAgregar.Click += new System.EventHandler(this.bAgregar_Click);
+            // 
+            // FrmLocalidades
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(679, 478);
+            this.Controls.Add(this.cmbDepartamento);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bVolver);
+            this.Controls.Add(this.tbNombreLocalidad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bCancelar);
+            this.Controls.Add(this.bGuardar);
+            this.Controls.Add(this.dgvLocalidades);
+            this.Controls.Add(this.bElimiar);
+            this.Controls.Add(this.bActualizar);
+            this.Controls.Add(this.bAgregar);
+            this.Font = new System.Drawing.Font("Cascadia Code SemiBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmLocalidades";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestión de Localidades";
+            this.Load += new System.EventHandler(this.FrmLocalidades_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLocalidades)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComboBox cmbDepartamento;
+        private Label label1;
+        private Button bVolver;
+        private TextBox tbNombreLocalidad;
+        private Label label3;
+        private Button bCancelar;
+        private Button bGuardar;
+        private DataGridView dgvLocalidades;
+        private Button bElimiar;
+        private Button bActualizar;
+        private Button bAgregar;
+    }
+}
---
+++ UI/FrmLocalidades.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class FrmLocalidades : Form
+    {
+        #region Inicializacion y Variables
+        private DataTable dtDatos;
+        private String formState = "init";
+        private DataLayer.Models.ViLocalidad localidadSeleccionada;
+        private DataLayer.Models.Departamento departamentoSeleccionado;
+        private List<DataLayer.Models.Departamento> departamentos;
+        private List<DataLayer.ComboboxItem> cmbBoxDepartamentosItems;
+        private bool loading;
+        public FrmLocalidades()
+        {
+            InitializeComponent();
+            departamentos = new List<DataLayer.Models.Departamento>();
+            cmbBoxDepartamentosItems = new List<DataLayer.ComboboxItem>();
+        }
+
+        private async void FrmLocalidades_Load(object sender, EventArgs e)
+        {
+            await DataLayer.Tasks.Authentication.BuildAuthHeaders(); // Esto se hará en el login, cuando exista.
+            List<DataLayer.Models.ViLocalidad> localidades = await DataLayer.Tasks.Localidad.listar();
+            CreateDataSource(localidades);
+
+
+            departamentos = await DataLayer.Tasks.Departamento.listar();  // listar las unidades para el cmbBox
+            cmbDepartamento.Items.Clear();
+            cmbDepartamento.Items.Add("Seleccione un departamento...");
+
+            foreach (DataLayer.Models.Departamento departamento in departamentos)
+            {
+                DataLayer.ComboboxItem cmbBoxItem = new DataLayer.ComboboxItem()
+                {
+                    ID = departamento.id_departamento,
+                    Text = departamento.nombre_departamento
+                };
+                cmbDepartamento.Items.Add(cmbBoxItem);
+                this.cmbBoxDepartamentosItems.Add(cmbBoxItem);
+            }
+
+            cmbDepartamento.SelectedIndex = 0;
+            cmbDepartamento.Enabled = false;
+        }
+        #endregion
+
+        #region Botones y Controles
+
+        private void bAgregar_Click(object sender, EventArgs e)
+        {
+            formState = "agregar";
+            ChangeState();
+        }
+
+        private void bActualizar_Click(object sender, EventArgs e)
+        {
+            formState = "actualizar";
+            ChangeState();
+        }
+
+        private async void bElimiar_Click(object sender, EventArgs e)
+        {
+            if (dgvLocalidades.SelectedRows.Count == 1)
+            {
+                int statusCode = await DataLayer.Tasks.Localidad.eliminar(localidadSeleccionada.id_localidad);
+
+                if (statusCode == 200)
+                    RefreshData();
+                formState = "init";
+                ChangeState();
+            }
+        }
+
+        private async void bGuardar_Click(object sender, EventArgs e)
+        {
+            switch (formState)
+            {
+                case "agregar":
+                    if (tbNombreLocalidad.Text != String.Empty && cmbDepartamento.SelectedIndex > 0)
+                    {
+                        //Crear e insertar la localidad
+                        DataLayer.Models.Localidad localidad = new DataLayer.Models.Localidad()
+                        {
+                            nombre_localidad = tbNombreLocalidad.Text,
+                            id_departamento = this.departamentoSeleccionado.id_departamento,
+                            usuario_registro = "dev" //TODO cambiar cuando exista login.
+                        };
+                        int statusCode = await DataLayer.Tasks.Localidad.insertar(localidad);
+
+                        if (statusCode == 201)
+                            RefreshData();
+                        formState = "init";
+                        ChangeState();
+                    }
+                    else
+                        MessageBox.Show("Ingrese todos los valores necesarios antes de guardar!", "Error!", MessageBoxButtons.OK);
+                    break;
+                case "actualizar":
+                    if (tbNombreLocalidad.Text != String.Empty && cmbDepartamento.SelectedIndex > 0)
+                    {
+                        DataLayer.Models.Localidad localidad = new DataLayer.Models.Localidad()
+                        {
+                            nombre_localidad = tbNombreLocalidad.Text,
+                            id_departamento = this.departamentoSeleccionado.id_departamento,
+                            usuario_registro = "dev" //TODO cambiar cuando exista login.
+                        };
+                        int statusCode = await DataLayer.Tasks.Localidad.actualizar(localidad, localidadSeleccionada.id_localidad);
+
+                        if (statusCode == 200)
+                            RefreshData();
+                        formState = "init";
+                        ChangeState();
+                    }
+                    else
+                        MessageBox.Show("Ingrese todos los valores necesarios antes de guardar!", "Error!", MessageBoxButtons.OK);
+                    break;
+            }
+        }
+
+        private void bCancelar_Click(object sender, EventArgs e)
+        {
+            formState = "init";
+            ChangeState();
+        }
+
+        private async void dgvLocalidades_SelectionChanged(object sender, EventArgs e)
+        {
+            if (this.loading == false && dgvLocalidades.SelectedRows.Count == 1)
+            {
+                int idLocalidadSeleccionada = Convert.ToInt32(dgvLocalidades.SelectedRows[0].Cells[0].Value);
+                this.localidadSeleccionada = await DataLayer.Tasks.Localidad.seleccionar(idLocalidadSeleccionada);
+                bActualizar.Enabled = true;
+                bElimiar.Enabled = true;
+                tbNombreLocalidad.Text = localidadSeleccionada.nombre_localidad;
+
+                //seleccionar el ComboBox segun la unidad
+                this.departamentoSeleccionado = await DataLayer.Tasks.Departamento.seleccionar(localidadSeleccionada.id_departamento);
+                int i = 1;
+                foreach (DataLayer.ComboboxItem item in cmbBoxDepartamentosItems)
+                {
+                    if (departamentoSeleccionado.id_departamento == item.ID)
+                        cmbDepartamento.SelectedIndex = i;
+                    i++;
+                }
+            }
+        }
+
+        private void cmbDepartamento_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.loading == false && cmbDepartamento.SelectedIndex > 0)
+            {
+                DataLayer.ComboboxItem selectedItem = cmbBoxDepartamentosItems[cmbDepartamento.SelectedIndex - 1];
+                foreach (DataLayer.Models.Departamento departamento in this.departamentos)
+                    if (departamento.id_departamento == selectedItem.ID)
+                        this.departamentoSeleccionado = departamento;
+            }
+            else if (cmbDepartamento.SelectedIndex == 0)
+                this.departamentoSeleccionado = new DataLayer.Models.Departamento();
+        }
+        #endregion
+
+        #region Metodos Auxiliares
+        private void ChangeState()
+        {
+            switch (formState)
+            {
+                case "init":
+                    tbNombreLocalidad.Enabled = false;
+                    cmbDepartamento.Enabled = false;
+
+                    bGuardar.Visible = false;
+                    bCancelar.Visible = false;
+                    bAgregar.Enabled = true;
+                    bActualizar.Enabled = false;
+                    bElimiar.Enabled = false;
+
+                    dgvLocalidades.ReadOnly = false;
+                    dgvLocalidades.ClearSelection();
+                    break;
+                case "agregar":
+                    tbNombreLocalidad.Enabled = true;
+                    tbNombreLocalidad.Text = String.Empty;
+                    cmbDepartamento.Enabled = true;
+                    cmbDepartamento.SelectedIndex = 0;
+
+                    bGuardar.Visible = true;
+                    bCancelar.Visible = true;
+                    bAgregar.Enabled = false;
+                    bActualizar.Enabled = false;
+                    bElimiar.Enabled = false;
+
+                    dgvLocalidades.ClearSelection();
+                    dgvLocalidades.ReadOnly = true;
+                    break;
+                case "actualizar":
+                    tbNombreLocalidad.Enabled = true;
+                    cmbDepartamento.Enabled = true;
+
+                    bGuardar.Visible = true;
+                    bCancelar.Visible = true;
+                    bAgregar.Enabled = false;
+                    bActualizar.Enabled = false;
+                    bElimiar.Enabled = false;
+
+                    dgvLocalidades.ReadOnly = true;
+                    break;
+            }
+        }
+
+        private void CreateDataSource(List<DataLayer.Models.ViLocalidad> localidades)
+        {
+            dtDatos = new DataTable();
+            dtDatos.Clear();
+
+            //metodo de ayuda para convertir las propiedades de una instancia en un dict.
+            Dictionary<string, object> propertyDict = DataLayer.Helpers.DictionaryFromInstance(localidades[0]);
+
+            //Agregamos columnas segun los atributos del objeto 
+            foreach (var item in propertyDict)
+                dtDatos.Columns.Add(item.Key);
+            //Agregamos filas
+            for (int i = 0; i < localidades.Count; i++)
+            {
+                propertyDict = DataLayer.Helpers.DictionaryFromInstance(localidades[i]);
+                DataRow _tempRow = dtDatos.NewRow();
+                foreach (var item in propertyDict)
+                    _tempRow[item.Key] = item.Value;
+                dtDatos.Rows.Add(_tempRow);
+            }
+            this.loading = true;
+            dtDatos.Columns["id_localidad"].SetOrdinal(0);
+            dtDatos.Columns["id_departamento"].SetOrdinal(1);
+            dtDatos.Columns["nombre_localidad"].SetOrdinal(2);
+
+            dgvLocalidades.DataSource = dtDatos;
+            dgvLocalidades.Columns[2].HeaderText = "Nombre Localidad";
+            dgvLocalidades.Columns[0].Width = 0;
+            dgvLocalidades.Columns[1].Width = 0;
+            dgvLocalidades.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dgvLocalidades.Refresh();
+            dgvLocalidades.ClearSelection();
+            this.loading = false;
+        }
+
+        private async void RefreshData()
+        {
+            List<DataLayer.Models.ViLocalidad> localidades = await DataLayer.Tasks.Localidad.listar();
+            CreateDataSource(localidades);
+        }
+        #endregion
+    }
+}
